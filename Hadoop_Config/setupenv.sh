c26e0d16b11516dff59b0bb24ec86e9a64448841
pmagmarques <143319199+pmagmarques@users.noreply.github.com>
2024-11-25 04:29:17 +0000

    t

---
+++ Hadoop_Config/setupenv.sh
@@ -0,0 +1,7 @@
+export HADOOP_HOME=/opt/hadoop 
+export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin 
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop 
+export YARN_CONF_DIR=$HADOOP_HOME/etc/hadoop
+
+export ZOOKEEPER_HOME=/opt/zookeeper 
+export PATH=$PATH:$ZOOKEEPER_HOME/bin
