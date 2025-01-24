89156345e0c88fcc161acf72e7198c7fd33bf256
pmagmarques <143319199+pmagmarques@users.noreply.github.com>
2025-01-24 18:38:26 +0000

    Update setupenv.sh

--- Hadoop_Config/setupenv.sh
+++ Hadoop_Config/setupenv.sh
@@ -1,7 +1,12 @@
-export HADOOP_HOME=/opt/hadoop 
-export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin 
-export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop 
-export YARN_CONF_DIR=$HADOOP_HOME/etc/hadoop
 
-export ZOOKEEPER_HOME=/opt/zookeeper 
-export PATH=$PATH:$ZOOKEEPER_HOME/bin
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
+export PATH=$PATH:$JAVA_HOME/bin
+export HADOOP_HOME=/opt/hadoop
+export HADOOP_PREFIX=$HADOOP_HOME
+
+export HIVE_HOME=/opt/hive
+
+export ZOOKEEPER_HOME=/opt/zookeeper
+
+export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/scbin:$HIVE_HOME/bin:$ZOOKEEPER_HOME/bin
+ 
